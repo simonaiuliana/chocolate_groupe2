34a593ae83a494d257c7c4cce946c2b5fc0e4d23
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-06-17 15:54:59 +0200

    update model Ingredient

--- model/Ingredient.php
+++ model/Ingredient.php
@@ -4,12 +4,14 @@ class Ingredient{
   private int $quantity;
   private string $unity;
   private string $name;
+  private ?string $img_url;
 
-  public function __construct(int $id, int $quantity, string $unity, string $name) {
+  public function __construct(int $id, int $quantity, string $unity, string $name, ?string $img_url) {
     $this->id = $id;
     $this->setQuantity($quantity);
     $this->setUnity($unity);
     $this->setName($name);
+    $this->setImgUrl($img_url);
   }
 
   // getters
@@ -25,6 +27,9 @@ class Ingredient{
   public function getName():string{
     return $this->name;
   }
+  public function getImgUrl():?string{
+    return $this->img_url;
+  }
 
   // setters
   public function setQuantity(int $quantity):self{
@@ -37,6 +42,10 @@ class Ingredient{
   }
   public function setName(string $name):self{
     $this->$name = $name;
+    return $this;
+  }
+  public function setImgUrl(string $img_url):self{
+    $this->$img_url = $img_url;
     return $this;
   }
 }
