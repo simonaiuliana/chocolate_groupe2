0a121a87088edc8f72b3991fc154e2724a7272da
Maxi Rojas <arnaldomrojas@outlook.com>
2024-06-23 16:44:04 +0200

    jquery

---
+++ membres/maxim/recetteMaxi.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".cardTitreMain").slideUp(5000).slideDown(4000);
+});
+
+$(document).ready(function(){
+    $(".description").hide()
+});
+$(document).ready(function(){
+    $(".description").delay(8000).show(8000)
+});
