988914ba066ebc7252e70d3a8e6323d5a84dc49a
Maxi Rojas <arnaldomrojas@outlook.com>
2024-06-24 12:33:16 +0200

    3photos

--- membres/maxim/recetteMaxi.js
+++ membres/maxim/recetteMaxi.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
-    $(".cardTitreMain").slideUp(5000).slideDown(4000);
+    $(".cardTitreMain").hide().slideDown(3000);
 });
 
 $(document).ready(function(){
     $(".description").hide()
 });
 $(document).ready(function(){
-    $(".description").delay(8000).show(8000)
+    $(".description").delay(1000).show(3000)
+});
+$(document).ready(function(){
+    $(".imgFondantRecette").hide().show(6000)
+});
+
+var bReady;
+
+$(document).ready(function(){
+  
+// This pauses the re-stacking until the expand animation is finished
+$(".ps-photoset").hover(function(){
+  bReady = false;
+  setTimeout(function(){
+    bReady = true;
+  }, 600);  
+});
+
+
+// 3 images //
+$(".ps-photo").hover(function(){
+  if (bReady == true){
+    $(".ps-photo").css("z-index", "auto");
+    $(this).css("z-index", "4");
+  } else {
+    // condole.log("stacking temporarily disabled")
+  }
 });
