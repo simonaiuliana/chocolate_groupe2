764e2d14e85cd7e2a271c7c4ffcdad5191b9b533
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-06-24 10:15:53 +0200

    recipe page

---
+++ membres/pomme/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="img/nav-img/th-r.jpeg">
+    <title>🍫 Délicieuse recette de glace au chocolat 🍫</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/recipes/glace-recipe.css">
+    <script src="https://unpkg.com/scrollreveal"></script>
+</head>
+
+<body>
+    <div id="place-holder-nav" style="display: none;">
+    </div>
+    <nav>
+        <div id="nav-links">
+            <a href="#" id="logo"><img src="img/nav-img/th-r.jpeg" alt="" width="50"></a>
+            <div id="container-burger">
+                <img src="img/nav-img/chocolat.png" alt="">
+            </div>
+            <div id="links">
+                <a href="#">Accueil</a>
+                <a href="javascript:void(0)" id="nav-recipes-link">Recettes<i class="fa-solid fa-arrow-down" style="margin-left: 10px;"></i></a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+        <div id="nav-recipes-container">
+            <ul id="nav-recipes" style="display: none;">
+                <a href="#"><li>Mousse au chocolat</li></a>
+                <a href="#"><li>Cake au chocolat</li></a>
+                <a href="#"><li>Fondant au chocolat</li></a>
+                <a href="#"><li>Tarte au chocolat</li></a>
+                <a href="#"><li>Cookies au chocolat</li></a>
+                <a href="#"><li>Glace au chocolat</li></a>
+                <a href="#"><li>Bûche de Noël au chocolat</li></a>
+                <a href="#"><li>Moelleux au chocolat</li></a>
+                <a href="#"><li>Truffes au chocolat</li></a>
+                <a href="#"><li>Macarons au chocolat</li></a>
+            </ul>
+            <div id="previews">
+            </div>
+        </div>
+        <div id="nav-choco-open-menu">
+            <img src="img/nav-img/openmenu.png" alt="">
+        </div>
+    </nav>
+    <div id="nav-choco">
+        <img src="img/nav-img/banner_top.png" alt="">
+    </div>
+   
+    <header class="header-text glace-banner" style="background-image: url('./chocopomme.jpg');">
+        <p>Choco Pomme</p>
+    </header>
+
+    <main>
+        <header class="general-infos">
+            <h1>Avant de commencer, voici ce dont vous devez savoir :</h1>
+            <div class="informations">
+                <div style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 20px;">
+                    <h3 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Informations utiles</h3>
+                    <p style="margin: 30px;">&nbsp;<i class="fa-solid fa-person" style="color: #e4e4e4;"></i> Servis pour: 4 personnes</p>
+                    <p style="margin: 30px;"><i class="fa-solid fa-clock" style="color: #e4e4e4;"></i> Préparation: 15 minutes</p>
+                    <p style="margin: 30px;"><i class="fa-solid fa-clock" style="color: #e4e4e4;"></i> Repos/cuisson: 15 minutes</p>
+                </div>
+                <div style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 20px;">
+                    <h3 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Ingrédients</h3>
+                    <ul style="margin: 30px;">
+                        <li><i class="fa-solid fa-receipt" style="color: #e4e4e4;"></i> 4 pommes</li>
+                        <li><i class="fa-solid fa-receipt" style="color: #e4e4e4;"></i> 100 g de chocolat noir pâtissier</li>
+                        <li><i class="fa-solid fa-receipt" style="color: #e4e4e4;"></i> 4 cuillères à café de sucre roux</li>
+                        <li><i class="fa-solid fa-receipt" style="color: #e4e4e4;"></i> 4 noisettes de beurre végétal</li>
+                        <li><i class="fa-solid fa-receipt" style="color: #e4e4e4;"></i> Un peu d’eau</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="sub-recipe" style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 30px auto;width: 80%;">
+            <h2 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Préparation des pommes : <br>( <i class="fa-solid fa-clock"></i> 5 min )</h2>
+            <div class="step" style="height: 2em;margin: 10px 0 10px 0">
+                <p style="margin-left: 2em;">1.<span style="margin-left: 1em;"></span>Préchauffez votre four à 180°C (thermostat 6).</p>
+            </div>
+            <div class="step" style="height: 2em;">
+                <p style="margin-left: 2em;">2.<span style="margin-left: 1em;"></span>Lavez les pommes et séchez-les avec du papier absorbant.</p>
+            </div>
+            <div class="step" style="height: 2em;">
+                <p style="margin-left: 2em;">3.<span style="margin-left: 1em;"></span>Retirez le trognon des pommes à l’aide d’un vide-pomme.</p>
+            </div>
+        </div>
+        <div class="sub-recipe" style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 30px auto;width: 80%;">
+            <h2 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Préparation du chocolat : <br>( <i class="fa-solid fa-clock"></i> 5 min )</h2>
+            <div class="step" style="height: 2em;margin: 10px 0 10px 0">
+                <p style="margin-left: 2em;">1.<span style="margin-left: 1em;"></span>Cassez le chocolat en petits morceaux.</p>
+            </div>
+            <div class="step" style="height: 2em;">
+                <p style="margin-left: 2em;">2.<span style="margin-left: 1em;"></span>Dans une casserole, faites fondre le chocolat avec un peu d’eau sur feu doux, en remuant régulièrement jusqu’à obtenir une sauce lisse.</p>
+            </div>
+        </div>
+        <div class="sub-recipe" style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 30px auto;width: 80%;">
+            <h2 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Cuisson des pommes : <br>( <i class="fa-solid fa-clock"></i> 15 min )</h2>
+            <div class="step" style="height: 2em;margin: 10px 0 10px 0">
+                <p style="margin-left: 2em;">1.<span style="margin-left: 1em;"></span>Placez les pommes dans un plat allant au four.</p>
+            </div>
+            <div class="step" style="height: 2em;">
+                <p style="margin-left: 2em;">2.<span style="margin-left: 1em;"></span>Remplissez chaque pomme évidée avec une cuillère à café de sucre roux et une noisette de beurre végétal.</p>
+            </div>
+            <div class="step" style="height: 2em;">
+                <p style="margin-left: 2em;">3.<span style="margin-left: 1em;"></span>Versez la sauce au chocolat fondue sur les pommes.</p>
+            </div>
+        </div>
+        <div class="sub-recipe" style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 30px auto;width: 80%;">
+            <h2 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Cuisson au four : <br>( <i class="fa-solid fa-clock"></i> 15 min )</h2>
+            <div class="step" style="height: 2em;margin: 10px 0 10px 0">
+                <p style="margin-left: 2em;">1.<span style="margin-left: 1em;"></span>Enfournez les pommes et laissez cuire pendant environ 15 minutes.</p>
+            </div>
+            <div class="step" style="height: 2em;">
+                <p style="margin-left: 2em;">2.<span style="margin-left: 1em;"></span>Les pommes doivent être tendres mais pas trop molles.</p>
+            </div>
+        </div>
+
+        <div class="sub-recipe" style="background-color: rgb(166 62 4);border-radius: 30px;color: #e4e4e4;margin: 30px auto;width: 80%;">
+            <h2 style="background-color: rgb(90, 35, 20);width: 100%;margin-top: 0;padding: 16px 0 16px 0;border-radius: 30px 30px 0 0;color: #e4e4e4;text-decoration: none;text-align: center;">Service : <br>( <i class="fa-solid fa-clock"></i> 15 min )</h2>
+            <div class="step" style="height: 2em;margin: 10px 0 10px 0">
+                <p style="margin-left: 2em;">1.<span style="margin-left: 1em;"></span>Servez les pommes chaudes, nappées de leur sauce au chocolat.</p>
+            </div>
+        </div>
+    </main>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="js/nav.js"></script>
+    <script>
+        ScrollReveal().reveal('.sub-recipe', { reset: true, delay: 100, duration: 1000, origin: "bottom", distance: "50px" });
+    </script>
+</body>
+
+</html>
