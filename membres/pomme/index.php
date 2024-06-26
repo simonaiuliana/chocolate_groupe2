1d587fc103a9a1729bdf186041d7679a3625f4cb
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-06-26 09:25:20 +0200

    fix navbar

--- membres/pomme/index.php
+++ membres/pomme/index.php
@@ -10,9 +10,9 @@
     <title>🍫 Délicieuse recette de glace au chocolat 🍫</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/recipes/glace-recipe.css">
     <link rel="stylesheet" href="css/footer/style.css">
     <link rel="stylesheet" href="css/footer/bootstrap.min.css">
+    <link rel="stylesheet" href="css/recipes/glace-recipe.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/footer/font-awesome.min.css" type="text/css">
     <script src="https://unpkg.com/scrollreveal"></script>
