60d1bbb232371a4d6c0af298ba5593e05da2a475
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-06-26 11:57:54 +0200

    add corner animation + refactor

--- membres/kevin/js/recipes/glace-recipe.js
+++ membres/kevin/js/recipes/glace-recipe.js
@@ -27,15 +27,58 @@ $(document).ready(function() {
         duration: 2000,
         once: true,
     });
+
+    /** Corner Chocolat */
+    const $cream1 = $('#ice-cream1');
+    const $cream2 = $('#ice-cream2');
+    const $corner = $('#corner');
+
+    function checkCornerIceCream(){
+        let posY = window.scrollY - 500;
+        posY = posY < 0 ? 0 : posY;
+
+        let posYCream = posY <= 600 ? posY : 600;
+        $cream1.css({
+            'top': posYCream
+        })
+
+        const actualTopCream2 = +$cream2.css('top').replace('px', '');
+        if(actualTopCream2 >= 540 && actualTopCream2 <= 640){
+            $cream2.css({
+                'top': posYCream + 40
+            })
+        }else{
+            let pos = 1000 - posYCream + 40;
+            if(posYCream >= 500)
+                pos = pos < 540 ? posYCream + 40 : pos;
+            
+            $cream2.css({
+                'top': pos + 'px'
+            })
+        }
+        
+        let posYCorner = posY <= 700 ? posY : 700;
+        $corner.css({
+            'top': 1400 - posYCorner + 20 + 'px'
+        })
+    }
+
+    addEventListener('scroll', checkCornerIceCream);
+    checkCornerIceCream();
+
+    /** end Corner Chocolat */
+
+    /** Comment */
+
     // Fonctionnalité de notation par étoiles
     $('.star-rating .fa-star').on('click', function() {
         var rating = $(this).data('rating');
         $('#rating').val(rating);
-        $('.star-rating .fa-star').each(function() {
+        $('.star-rating.position-relative > .fa-star').each(function() {
             if ($(this).data('rating') <= rating) {
-                $(this).addClass('checked');
+                $(this).removeClass('fa-regular').addClass('fa').addClass('checked');
             } else {
-                $(this).removeClass('checked');
+                $(this).removeClass('fa').addClass('fa-regular').removeClass('checked');
             }
         });
     });
@@ -63,7 +106,7 @@ $(document).ready(function() {
         // Réinitialiser le formulaire
         $('#comment-form')[0].reset();
         $('#rating').val(0);
-        $('.star-rating .fa-star').removeClass('checked');
+        $('.star-rating .fa-star').removeClass('fa').addClass('fa-regular').removeClass('checked');
     });
 
     // Fonction pour générer les étoiles
@@ -93,4 +136,5 @@ $(document).ready(function() {
         };
         return text.replace(/[&<>"'`=\/]/g, function(m) { return map[m]; });
     }
+    /** end comment */
 });
